f7ee6214bdd682e37eba3f48070b2b5860de8045
suwakouhei <s.k.293758@icloud.com>
2021-06-28 16:02:18 +0900

    ビューファイルの配置２

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'item', to: 'items#index'
+  root to: 'items#index'
 end
