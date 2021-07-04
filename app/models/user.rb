51c852485a67703678a6804eb07e5f1d7d129e60
suwakouhei <s.k.293758@icloud.com>
2021-07-04 22:05:44 +0900

    パスワードに英数字のみ保存するためのコードを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+
          validates :nickname, presence: true
+         validates :password,format: { with: VALID_PASSWORD_REGEX}
          validates :last_name, presence: true
          validates :first_name, presence: true
          validates :last_name_katakana, presence: true
