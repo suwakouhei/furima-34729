9a2fd72f5372fb554a69b3676934f659f4848e3a
suwakouhei <s.k.293758@icloud.com>
2021-07-05 20:22:39 +0900

    factorybotの値を変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name                   {"諏訪"}
+    last_name                    {"晃平"}
+    first_name_katakana          {"スワ"}
+    last_name_katakana           {"コウヘイ"}
+    birthday                     {Faker::Date.birthday}
   end
 end
