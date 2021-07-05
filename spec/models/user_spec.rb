35aa7b66bf02a720389a9dbf75d7a5a79b258335
suwakouhei <s.k.293758@icloud.com>
2021-07-05 20:24:17 +0900

    テストコードの記述、正しく動くか一つ一つ確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,31 +10,112 @@ RSpec.describe User, type: :model do
 
     context '新規登録がうまくいくとき' do
 
-     - ニックネームが必須であること
-     - メールアドレスが必須であること
-     - メールアドレスが一意性であること
-     - メールアドレスは、@を含む必要があること
-     - パスワードが必須であること
-     - パスワードは、6文字以上での入力が必須であること（6文字が入力されていれば、登録が可能なこと）
-     - パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）
-     - パスワードは、確認用を含めて2回入力すること
-     - パスワードとパスワード（確認用）は、値の一致が必須であること
-  
+      it "nicknameとemail、passwordとpassword_confirmation,first_nameとlast_name,first_name_katakanaとlast_name_katakana,birthdayが存在するとき" do
+        expect(@user).to be_valid
+      end
+
     end
 
     context '新規登録がうまくいかないとき' do
 
-     -ニックネームが必須であること
-     - メールアドレスが必須であること
-     - メールアドレスが一意性であること
-     - メールアドレスは、@を含む必要があること
-     - パスワードが必須であること
-     - パスワードは、6文字以上での入力が必須であること（6文字が入力されていれば、登録が可能なこと）
-     - パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）
-     - パスワードは、確認用を含めて2回入力すること
-     - パスワードとパスワード（確認用）は、値の一致が必須であること
+     it "ニックネームが存在しないとき" do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+
+     end
+
+     it "メールアドレスが存在しないとき" do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+     end
+
+     it "メールアドレスが一意性でないとき" do
+      @user.email = 'aaaa1111@gmail.com'
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+     end
+
+     it "メールアドレスは、@を含まないとき" do
+      @user.email = 'aaaaaa.gmail.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+     end
+
+      it "パスワードが存在しないとき" do
+        @user.password = ''
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank", "Password is invalid")
+      end
+
+      it "パスワードは、6文字以上でないとき（6文字が入力されていれば、登録が可能なこと）" do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)", "Password is invalid")
+      end
+
+      it "パスワードは、半角英数字混合でないとき（半角英数字が混合されていれば、登録が可能なこと）" do
+        @user.password = 'aaaaaaaa'
+        @user.password_confirmation = 'aaaaaaaa'
+        @user.valid? 
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
+      it "パスワードは、確認用を含めて2回入力しないとき" do 
+        @user.password = 'aaaa1111'
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it "パスワードとパスワード（確認用）の値が一致しないとき" do
+        @user.password = 'aaaa1111'
+        @user.password_confirmation = 'bbbb1111'
+        @user.valid? 
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
 
 
+      it "ユーザー本名は、名字と名前が存在しない" do
+        @user.first_name = ''
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank", "First name can't be blank")
+      end
+
+      it "ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力していないとき" do
+        @user.first_name = '12'
+        @user.last_name = '12'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid", "First name is invalid")
+      end
+
+      it "ユーザー本名のフリガナは、名字と名前が存在しない" do 
+        @user.first_name_katakana = ''
+        @user.last_name_katakana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name katakana can't be blank", "First name katakana can't be blank")
+      end
+
+  it "ユーザー本名のフリガナは、全角（カタカナ）での入力していないとき" do
+        @user.first_name_katakana = 'すわ'
+        @user.last_name_katakana = 'こうへい'
+        @user.valid?
+      end
+
+      it "生年月日がそれぞれ選択されていないとき" do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+    end
   end
 
 end
