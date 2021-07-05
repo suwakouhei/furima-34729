5810055b1c21d77e3d2a6e47777e332485ef372f
suwakouhei <s.k.293758@icloud.com>
2021-07-05 21:51:14 +0900

    名前と名字の値が逆だったため変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
-    first_name                   {"諏訪"}
-    last_name                    {"晃平"}
-    first_name_katakana          {"スワ"}
-    last_name_katakana           {"コウヘイ"}
+    first_name                   {"晃平"}
+    last_name                    {"諏訪"}
+    first_name_katakana          {"コウヘイ"}
+    last_name_katakana           {"スワ"}
     birthday                     {Faker::Date.birthday}
   end
 end
