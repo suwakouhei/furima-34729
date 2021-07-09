d43d8340f4d17cbc1fbbd3bd60e366ae650e9214
suwakouhei <s.k.293758@icloud.com>
2021-07-09 10:38:18 +0900

    情報の受け取り変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to root_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:item_name,:item_description,:item_category_id,:item_status_id,:delivery_burden_id,:prefectures_id,:shipping_days_id,:item_price,)
+  end
+
 end
