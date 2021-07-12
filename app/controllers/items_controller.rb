81cf26465c74ac02d8b9356bb1ffb22a1cc0da8a
suwakouhei <s.k.293758@icloud.com>
2021-07-12 21:41:57 +0900

    保存できた時とできない時で条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,19 @@ class ItemsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
     redirect_to root_path
+    
+    else
+      render :new
+      
+    end
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:item_name,:item_description,:item_category_id,:item_status_id,:delivery_burden_id,:prefectures_id,:shipping_days_id,:item_price,)
+    params.require(:list).permit(:image, :item_name,:item_description,:item_category_id,:item_status_id,:delivery_burden_id,:prefecture_id,:shipping_day_id,:item_price).merge(user_id: current_user.id)
   end
 
 end
