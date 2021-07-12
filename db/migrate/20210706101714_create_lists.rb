b5f950832011441410ce4507dd89de54a993c614
suwakouhei <s.k.293758@icloud.com>
2021-07-12 21:39:28 +0900

    複数形を単数形に変更

--- db/migrate/20210706101714_create_lists.rb
+++ db/migrate/20210706101714_create_lists.rb
@@ -6,10 +6,10 @@ class CreateLists < ActiveRecord::Migration[6.0]
       t.integer :item_category_id, null: false
       t.integer :item_status_id, null: false
       t.integer :delivery_burden_id, null: false
-      t.integer :prefectures_id, null: false
-      t.integer :shipping_days_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :shipping_day_id, null: false
       t.integer :item_price, null: false
-      
+      t.references :user, null:false, foreign_key: true
 
       t.timestamps
     end
