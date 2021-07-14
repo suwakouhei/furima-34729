58e91a639331c8c572278e3586b62413a51551de
suwakouhei <s.k.293758@icloud.com>
2021-07-14 18:37:41 +0900

    数値に''で囲うことを無くした。

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe List, type: :model do
   before do
     @list = FactoryBot.build(:list)
+    
   end
 
   describe '出品登録' do
@@ -32,7 +33,7 @@ RSpec.describe List, type: :model do
       end
 
       it 'カテゴリーの選択欄でidに1が選択されている場合は出品できない' do
-        @list.item_category_id = '1'
+        @list.item_category_id = 1
         @list.valid?
         expect(@list.errors.full_messages).to include("Item category can't be blank")
       end
@@ -44,7 +45,7 @@ RSpec.describe List, type: :model do
       end
 
       it '商品の状態の選択欄でidに1が選択されている場合は出品できない' do
-        @list.item_status_id = '1'
+        @list.item_status_id = 1
         @list.valid?
         expect(@list.errors.full_messages).to include("Item status can't be blank")
       end
@@ -56,7 +57,7 @@ RSpec.describe List, type: :model do
       end
 
       it '配送料の負担の選択欄でidに1が選択されている場合は出品できない' do
-        @list.delivery_burden_id = '1'
+        @list.delivery_burden_id = 1
         @list.valid?
         expect(@list.errors.full_messages).to include("Delivery burden can't be blank")
       end
@@ -68,7 +69,7 @@ RSpec.describe List, type: :model do
       end
 
       it '発送元の地域の選択欄でidに1が選択されている場合は出品できない' do
-        @list.prefecture_id = '1'
+        @list.prefecture_id = 1
         @list.valid?
         expect(@list.errors.full_messages).to include("Prefecture can't be blank")
       end
@@ -80,7 +81,7 @@ RSpec.describe List, type: :model do
       end
 
       it '発送までの日数の選択欄でidに1が選択されている場合は出品できない' do
-        @list.shipping_day_id = '1'
+        @list.shipping_day_id = 1
         @list.valid?
         expect(@list.errors.full_messages).to include("Shipping day can't be blank")
       end
@@ -116,7 +117,7 @@ RSpec.describe List, type: :model do
       end
 
       it '商品価格が10_000_000円以上では出品できない' do
-        @list.item_price = '10000000000'
+        @list.item_price = 10000000000
         @list.valid?
         expect(@list.errors.full_messages).to include('Item price must be less than 10000000')
       end
