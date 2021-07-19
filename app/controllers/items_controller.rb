f17a18d05886b9afd9fe6b2eba5926abdb1632e2
suwakouhei <s.k.293758@icloud.com>
2021-07-19 15:59:40 +0900

    before_actionを使って実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_session, only: :edit
+  before_action :move_to_root, only: :edit
   def index
     @lists = List.all.order('created_at DESC')
   end
@@ -43,4 +45,18 @@ class ItemsController < ApplicationController
     params.require(:list).permit(:image, :item_name, :item_description, :item_category_id, :item_status_id, :delivery_burden_id,
                                  :prefecture_id, :shipping_day_id, :item_price).merge(user_id: current_user.id)
   end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end 
+  end
+
+  def move_to_root
+    unless current_user == current_user.nickname
+      redirect_to root_path
+    end 
+  end
+  
+
 end
