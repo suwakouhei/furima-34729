ce1a973d920085cd249276596915ac1d0a299a48
suwakouhei <s.k.293758@icloud.com>
2021-07-19 20:13:09 +0900

    Revert "Revert ":destroyアクション追加""
    This reverts commit 1c9f4948b41655b496b251f16ec9b3a047bbb14d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create,:edit,:update]
-  before_action :one_get_list, only: [:show,:edit,:update]
-  before_action :move_to_root, only: :edit
+  before_action :one_get_list, only: [:show,:edit,:update,:destroy]
+  before_action :move_to_root, only: [:edit,:destroy]
 
 
   def index
@@ -40,6 +40,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @list.destroy
+    redirect_to root_path
+  end
+
 
   private
 
