33d3f73f730f28e9dbe1e34ec8ff741b0bb20c80
suwakouhei <s.k.293758@icloud.com>
2021-07-19 17:10:14 +0900

    authenticate_user!にupdateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create,:edit]
+  before_action :authenticate_user!, only: [:new, :create,:edit,:update]
   before_action :one_get_list, only: [:show,:edit,:update]
   before_action :move_to_root, only: :edit
 
