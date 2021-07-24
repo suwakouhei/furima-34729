438e1730327fbf08c62b3346bedaf416e36aa366
suwakouhei <s.k.293758@icloud.com>
2021-07-24 18:15:23 +0900

    ルーティング指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-
+  get 'items/orders' , to:'orders#index'
+  resources :items do
+    resources :orders
+  end
 end
