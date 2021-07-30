0d325a145290f2b4ba3cd0b0faf2623946ae781e
suwakouhei <s.k.293758@icloud.com>
2021-07-30 10:56:47 +0900

    card.js追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,24 @@
 class OrdersController < ApplicationController
   def index
-    
+    @purchase_address = PurchaseStreetAdress.new
   end
+  def create
+    @purchase_address = PurchaseStreetAdress.new(purchase_address_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
 
 end
+
+private
+
+def purchase_address_params
+  params.require(:purchase_address).permit(:postal_code, :prefectures_id, :municipality, :address, :phone_number,:building_name).merge(user_id: current_user.id, list_id: current_list.id)
+end
+
+
