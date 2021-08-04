e822eed64ace3824f1228ae77d728a704a13804c
suwakouhei <s.k.293758@icloud.com>
2021-08-04 11:03:59 +0900

    factorytobの値変更

--- spec/factories/purchase_street_addresses.rb
+++ spec/factories/purchase_street_addresses.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :purchase_street_address do
     token { 'tok_abcdefghijk00000000000000000' }
     postal_code { '165-0033' }
-    prefectures_id { '1' }
+    prefectures_id { 2 }
     municipality { '横浜市' }
     address { '青山1-1-1' }
     phone_number { '09012345678' }
