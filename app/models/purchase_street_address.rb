e23620f935748f4ee5def26b5c39a4458e2d6cd6
suwakouhei <s.k.293758@icloud.com>
2021-08-04 10:19:08 +0900

    バリデーション 追加

--- app/models/purchase_street_address.rb
+++ app/models/purchase_street_address.rb
@@ -2,12 +2,15 @@ class PurchaseStreetAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :list_id, :user_id, :token
 
+  haihun = /[0-9]{3}-[0-9]{4}/
+  denwa = /\A\d{11}\z/
+  
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: haihun }
     validates :prefectures_id
     validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: denwa }
     validates :user_id
     validates :list_id
     validates :token
