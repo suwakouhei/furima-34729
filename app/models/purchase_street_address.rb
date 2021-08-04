7f17f89eab1ffa823f21eed8cb667030d347eea5
suwakouhei <s.k.293758@icloud.com>
2021-08-04 11:03:41 +0900

    validation追加

--- app/models/purchase_street_address.rb
+++ app/models/purchase_street_address.rb
@@ -7,7 +7,7 @@ class PurchaseStreetAddress
   
   with_options presence: true do
     validates :postal_code, format: { with: haihun }
-    validates :prefectures_id
+    validates :prefectures_id,numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: denwa }
@@ -18,6 +18,6 @@ class PurchaseStreetAddress
   def save
     purchase = Purchase.create(user_id: user_id, list_id: list_id)
     StreetAddress.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address,
-                         phone_number: phone_number)
+                         phone_number: phone_number,purchase_id: purchase.id)
   end
 end
